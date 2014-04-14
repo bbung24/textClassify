0558803afeedd31101343360a3bc6fdbcc6c1d37
Inho Yong <bbung24@gmail.com>
2014-04-14 13:07:47 -0400

    Fixed stop-words because it seems there was an error at the end of matrix

--- code/featurize.py
+++ code/featurize.py
@@ -17,7 +17,7 @@ def featurize(train_articles, test_articles):
                     'because','been','but','by','can','cannot','could','dear',
                     'did','do','does','either','else','ever','every','for',
                     'from','get','got','had','has','have','he','her','hers',
-                    'him','his','how','however','i','if','in','into','is','it',
+                    'him','his','how','however','if','in','into','is','it',
                     'its','just','least','let','like','likely','may','me',
                     'might','most','must','my','neither','no','nor','not','of',
                     'off','often','on','only','or','other','our','own','rather',
@@ -25,7 +25,7 @@ def featurize(train_articles, test_articles):
                     'than','that','the','their','them','then','there','these',
                     'they','this','to','too','us','wants','was','we','were',
                     'what','when','where','which','while','who','whom','why',
-                    'will','with','would','yet','you','your',"'s", "'ve"]
+                    'will','with','would','yet','you','yours', 'we']
         
             
         # Generate all the unique words in the entire document
